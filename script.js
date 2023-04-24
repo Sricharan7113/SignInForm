1e9afd20c4df986805292ad0b29048c0e80c6cef
Sricharan N <90692839+Sricharan7113@users.noreply.github.com>
2023-04-24 22:51:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
         if(Username.value=="" || UserName.vaule==null || UserName.value==undefined 
         ||Password.value=="" || Password.vaule==null || Password.value==undefined)
         {
-            document.getElementById("FormContainer").style.backgroundColor='rgba(200, 90, 90, 0.34)';
+            document.getElementById("FormContainer").style.backgroundColor='rgba(247, 103, 103, 0.196)';
             document.getElementById("FormContainer").style.boxShadow='0px 1px 2px 3px rgba(216, 37, 5, 0.466)';
         }
     })
